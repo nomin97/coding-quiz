649698960695a8abb7cf24bce87d6a01cfe92db5
Nomin Myagmarsuren <nominmyagmarsuren@gmail.com>
2023-07-27 00:31:44 -0600

    added quiz questions and answers

--- assets/script.js
+++ assets/script.js
@@ -10,3 +10,45 @@
 // WHEN the game is over
 // THEN I can save my initials and score
 
+var quizQA = [
+    {
+        question: "What is the right way to use CamelCase in JavaScript?",
+        a: "FirstQuestionAnswer",
+        b: "firstQuestionAnswer",
+        c: "FirstquestionAnswer",
+        d: "firstquestionanswer",
+        correct: "b",
+    },
+    {
+        question: "Which of the following adds an element to the begining of an array?",
+        a: "unshift",
+        b: "pop",
+        c: "shift",
+        d: "concat",
+        correct: "a",
+    },
+    {
+        question: "When you think of arrays, you should think...",
+        a: "curly brackets {}",
+        b: "parenthesis()",
+        c: "quote marks' '",
+        d: "square brackets []",
+        correct: "d",
+    },
+    {
+        question: "How do you turn a string into an array?",
+        a: "string.array(delimiter, limit)",
+        b: "array.string(delimiter, limit)",
+        c: "string.seperate(delimiter, limit)",
+        d: "string.split(delimiter, limit)",
+        correct: "d",
+    },
+    {
+        question: "How do you get user info?",
+        a: "prompt",
+        b: "call",
+        c: "ask",
+        d: "request",
+        correct: "a",
+    },
+];
